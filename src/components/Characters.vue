c0a9afd904657cd6eebd01dc8127569be8f540f0
Khalil Tarabulsi <kh476379@dal.ca>
2022-10-30 08:23:11 -0300

    added Characters component

---
+++ src/components/Characters.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+
+    <v-container class="my-5">
+
+      <!-- Marvel Yearbook Collection -->
+      <v-layout row wrap>
+      </v-layout>
+
+    </v-container>
+
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'CharactersVue',
+  data() {
+    return {
+      characters: [],
+      thumbnailSize: 'standard_fantastic.jpg',  // From the documentation
+      features: ['name', 'stories', 'comics', 'series'],
+      featureMap: { 'name': 'name', 'stories': 'title', 'comics': 'title', 'series': 'title' }
+
+    };
+  },
+  methods: {
+    getFeatureName: function (feature) {
+      return this.featureMap[feature];
+    },
+    changeFeature(feature) {
+
+      const url = `http://0.0.0.0:5000/${feature}`;
+      let attribute = this.getFeatureName(feature);
+      axios.get(url)
+        .then((result) => {
+          this.characters = []
+          result.data.forEach(element => {
+            let thumbnail = (`${element.thumbnail.path}/${this.thumbnailSize}`)
+            element['thumbnail'] = thumbnail
+            element['feature'] = element[attribute]
+            this.characters.push(element);
+          });
+        }).catch((error) => {
+          alert(error);
+        });
+
+    }
+  },
+};
+</script>
+
+<style>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,14 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <CharactersVue />
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue';
-
+import CharactersVue from '@/components/Characters.vue';
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld,
+    CharactersVue,
   },
-};
+}
 </script>
